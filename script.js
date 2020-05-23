378341386cf3247e7843599ee5522328edb631a9
T-alabi <talabi@ucvts.org>
2020-05-23 19:27:10 -0400

    home page done

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function UserName(){
   document.getElementById("NamePromt").style.display = "block";
-console.log("clicked");
+document.getElementById("next").style.display = "none";
+}
+function GetName(){
+  var name = document.getElementById("firstName").value;
+  document.getElementById("NamePromt").style.display = "none";
+  document.getElementById("response").innerHTML = "Welcome " + name + ", to the mission";
+  document.getElementById("next2").style.display = "block";
+}
+function showStart(){
+  document.getElementById("startlevel1").style.display = "block";
+  console.log("clicked");
 }
