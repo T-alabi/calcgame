a17f4b1caf61d21aa3d85655884fca02cf69162a
T-alabi <talabi@ucvts.org>
2020-05-24 18:18:26 -0400

    code cleaned and organized

---
+++ scripts/standards.js
@@ -0,0 +1,8 @@
+var nav =   '<div class="navbar navbar-expand-sm " style="background-color: #768a72;">' +
+    '<a href="home.html" class="text-decoration-none" id="welcome">Name here</a>' +
+    '<a href="Limits.html" id="navLink">Limits</a>' +
+    '<a href="" id="navLink">Derivatives</a>' +
+    '<a href="" id="navLink">Integrals</a>' +
+  '</div>'
+            ;
+document.getElementById("replaceNav").innerHTML = nav;
